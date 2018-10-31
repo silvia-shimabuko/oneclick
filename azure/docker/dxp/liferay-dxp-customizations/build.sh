8ef23f0a9a2942a7d29bce01e59f929ed8782f1d
silvia-shimabuko <silvia.shimabuko@objective.com.br>
2018-10-31 13:29:46 -0300

    [TOOLS-1741]files used by oneclick config - CHANGES

--- azure/docker/dxp/liferay-dxp-customizations/build.sh
+++ azure/docker/dxp/liferay-dxp-customizations/build.sh
@@ -4,7 +4,10 @@ set -e
 cd $(dirname $(readlink -f $0))
 
 VERSION=$1
+VERSION_NAME_DXP=$2
 
 source commons.sh
 
-docker build --tag $FULLNAME:$VERSION --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10-hotfix1 --tag $FULLNAME --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10-hotfix1 .
+#docker build --tag $FULLNAME:$VERSION --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10-hotfix1 --tag $FULLNAME --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10-hotfix1 .
+echo "docker build --tag $FULLNAME:$VERSION --build-arg DXP_IMAGE=liferay/liferay-dxp:$VERSION_NAME_DXP-hotfix1 ."
+docker build --tag $FULLNAME:$VERSION --build-arg DXP_IMAGE=liferay/liferay-dxp:$VERSION_NAME_DXP-hotfix1 .
--- azure/docker/dxp/liferay-dxp-patches/build-image.sh
+++ azure/docker/dxp/liferay-dxp-patches/build-image.sh
@@ -4,5 +4,9 @@ set -e
 
 cd $(dirname $(readlink -f $0))
 
-docker build --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10 --tag liferay/liferay-dxp:7.1.10-hotfix1 .
+VERSION_NAME_DXP=$1
+
+#docker build --build-arg DXP_IMAGE=liferay/liferay-dxp:7.1.10 --tag liferay/liferay-dxp:7.1.10-hotfix1 .
+echo "docker build --build-arg DXP_IMAGE=liferay/liferay-dxp:$VERSION_NAME_DXP --tag liferay/liferay-dxp:$VERSION_NAME_DXP-hotfix1 ."
+docker build --build-arg DXP_IMAGE=liferay/liferay-dxp:$VERSION_NAME_DXP --tag liferay/liferay-dxp:$VERSION_NAME_DXP-hotfix1 .
 
